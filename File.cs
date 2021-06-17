fb5728afcdaddd9b279c12ac9679fa8c67b04f43
Viktor Adrian Mouton <viktor@mterms.com>
2021-06-17 11:38:29 +0200

    Setup done

---
+++ File.cs
@@ -0,0 +1,14 @@
+
+class File
+{
+    private string noe;
+    public File(string newThing)
+    {
+        this.noe = newThing;
+    }
+
+    public string getNoe()
+    {
+        return this.noe;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PDFConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File Ludvig = new File("Solan");
+            Console.WriteLine(Ludvig.getNoe());
+        }
+    }
+}
